a1f176e1ad7e3dc6a1b843997b7e42212234751e
Emesson <emerssonkavallcante@gmail.com>
2021-10-06 16:04:41 -0300

    Adicionei algumas opções

--- src/visao/TelaRh.java
+++ src/visao/TelaRh.java
@@ -5,17 +5,31 @@
  */
 package visao;
 
+import Dao.Conecxao;
+import java.sql.*;
+
+
 /**
  *
  * @author emesson
  */
 public class TelaRh extends javax.swing.JInternalFrame {
+    Conecxao conn = new Conecxao();
+    
+    // Abre conexão
+    Connection conexao = null;
+    // Prepara a query
+    PreparedStatement pst = null;
+    // Exibe resultado da conexão feita com o banco
+    ResultSet rs = null;
 
     /**
      * Creates new form TelaRh
      */
-    public TelaRh() {
+    public TelaRh() throws SQLException {
         initComponents();
+        conexao = conn.abricConecxao();
+
     }
 
     /**
@@ -69,6 +83,12 @@ public class TelaRh extends javax.swing.JInternalFrame {
 
         jLabel4.setText("Status");
 
+        txtBuscaNome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtBuscaNomeActionPerformed(evt);
+            }
+        });
+
         selBuscaCargo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Administradores", "Caixa", "Gerentes", "Almoxarifes", "Recursos Humanos" }));
         selBuscaCargo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -77,6 +97,11 @@ public class TelaRh extends javax.swing.JInternalFrame {
         });
 
         btnPesquisar.setText("Pesquisar");
+        btnPesquisar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisarActionPerformed(evt);
+            }
+        });
 
         selBuscaStatus.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Ativo", "Inativo", "Férias", "Despedido" }));
 
@@ -175,6 +200,19 @@ public class TelaRh extends javax.swing.JInternalFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_selBuscaCargoActionPerformed
 
+    private void txtBuscaNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtBuscaNomeActionPerformed
+        // TODO add your handling code here:        
+    }//GEN-LAST:event_txtBuscaNomeActionPerformed
+
+    private void btnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarActionPerformed
+        // TODO add your handling code here:
+        this.txtBuscaNome.setText("Hello world!");
+        
+        
+        // Quando O BOTÃO de busca for pressionado
+        Connection conexao = null;
+        
+    }//GEN-LAST:event_btnPesquisarActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnPesquisar;
